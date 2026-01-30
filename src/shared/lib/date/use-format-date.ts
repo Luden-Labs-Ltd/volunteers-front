0b026c1237713114b37af2cff07f73d3bd7784d2
bakuga <maximgancharik@mail.ru>
2026-01-30 13:35:54 +0300

    fix(ui): fix formatDate

--- src/shared/lib/date/use-format-date.ts
+++ src/shared/lib/date/use-format-date.ts
@@ -1,26 +1,11 @@
-export const formatDate = (input: string | Date, locale = 'en-US'): string => {
-    if (!input) return "";
+export const formatDate = (dateString: string | Date, locale = 'en-US'): string => {
+    if (!dateString) return "";
 
-    let date: Date | null =null
-
-    if (input instanceof Date) {
-        date = input
-    } else if (typeof input === "number") {
-        date = new Date(input)
-    } else if (typeof input === "string") {
-        let value = input.trim()
-        value = value.replace(/\s*\(.*?\)\s*$/, "");
-        date = new Date(value);
-    }
-
-    if (!date || isNaN(date.getTime())) {
-        console.error("Invalid date:", input);
-        return "Invalid date";
-    }
+    const date = new Date(dateString);
 
     return date.toLocaleDateString(locale, {
         weekday: 'long',
         month: 'long',
-        day: 'numeric',
+        day: 'numeric'
     });
 };
