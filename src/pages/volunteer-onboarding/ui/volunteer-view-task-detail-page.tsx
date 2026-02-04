c1a2d625ce5167a4cc69395aee9e98541c14d0aa
Philip Veremeichik <p.veremeichik@richbrains.net>
2026-02-04 13:19:04 +0300

    add revalidate

--- src/pages/volunteer-onboarding/ui/volunteer-view-task-detail-page.tsx
+++ src/pages/volunteer-onboarding/ui/volunteer-view-task-detail-page.tsx
@@ -160,7 +160,7 @@ export const VolunteerViewTaskDetailsPage = () => {
                     <p className="text-textGray font-normal text-sm mb-1">
                       {t('volunteerTask.details.descriptionTitle')}
                     </p>
-                    <p className="text-textGray font-medium whitespace-pre-line">
+                    <p className="text-textGray font-medium whitespace-pre-line break-words">
                       {task.description}
                     </p>
                   </div>
